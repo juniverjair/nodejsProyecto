6dcef9e3aabc1d2ff91e23293e065e3d3fe5e0fc
Juniver Román Macías <jjroman@espol.edu.ec>
2021-08-09 22:49:40 -0500

    socket.io

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//const port = 3000;
+
+var http = require('http');
 var app = express();
-const port = 3000;
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,10 +24,26 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, '/')))
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
